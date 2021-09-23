e257542750961bb699dd39dae37517e1602c2b9f
taku0709 <ai3.ca7@gmail.com>
2021-09-23 16:55:55 +0000

    [finish]4-課題

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'homes#top'
+  get 'about' => 'homes#about'
   resources :books
   resources :users
   resources :books, only: [:new, :create, :index, :show, :destroy]
