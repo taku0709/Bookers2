2673b82caf5e11a79675387abe981f76ed7b165f
taku0709 <ai3.ca7@gmail.com>
2021-09-24 14:45:54 +0000

    [finish]4-課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,19 @@
 class BooksController < ApplicationController
-  
+
   def new
   end
-  
+
   def create
   end
-  
+
   def index
+    @book = Book.ne
   end
-  
+
   def show
   end
-  
+
   def destroy
   end
-  
+
 end
